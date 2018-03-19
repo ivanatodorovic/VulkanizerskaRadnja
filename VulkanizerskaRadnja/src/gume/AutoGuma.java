4c4b9d48114628f0b456335e20ef08e50b962f4e
ivanatodorovic <ititodorovic1998@gmail.com>
2018-03-19 06:21:14 +0100

    Dokumentovana je klasa AutoGuma. Napravljena je dokumentacija za atribute i metode.

--- VulkanizerskaRadnja/src/gume/AutoGuma.java
+++ VulkanizerskaRadnja/src/gume/AutoGuma.java
@@ -1,15 +1,37 @@
 package gume;
-
+/**
+ *Klasa predstavlja automobilsku gumu 
+ *sa svim svojim atributima, nazivom, precnikom, sirinom i visinom 
+ * @author Ivana Todorovic
+ * @version 0.0.1
+ *
+ */
 public class AutoGuma {
+/**
+ * Naziv marke modela automobilske gume
+ */
 	private String markaModel = null;
+	/**
+	 * Precnik automobilske gume
+	 */
 	private int precnik = 0;
+	/**
+	 * Sirina automobilske gume
+	 */
 	private int sirina = 0;
+	/**
+	 * Visina automobilske gume 
+	 */
 	private int visina = 0;
 	
 	public String getMarkaModel() {
 	return markaModel;
 	}
-	
+	/**
+	 * Metoda za postavljanje naziva marke modela auto gume.
+	 * @param markaModel
+	 * @throws java.lang.RuntimeException ukoliko ne parametar null ili ima manje od 3 znaka
+	 */
 	public void setMarkaModel(String markaModel) {
 	if (markaModel==null || markaModel.length()<3)
 	throw new RuntimeException("Morate uneti marku i model");
@@ -19,7 +41,11 @@ public class AutoGuma {
 	public int getPrecnik() {
 	return precnik;
 	}
-	
+	/**
+	 * Postavlja vrednost precnika gume.
+	 * @param precnik
+	 * @throws java.lang.RuntimeException za sve vrednosti precnika koje su van opsega 13-22
+	 */
 	public void setPrecnik(int precnik) {
 	if (precnik < 13 && precnik > 22)
 	throw new RuntimeException("Precnik van opsega");
@@ -30,6 +56,11 @@ public class AutoGuma {
 	return sirina;
 	}
 	
+	/**
+	 * Postavlja vrednost sirine gume.
+	 * @param sirina
+	 * @throws java.lang.RuntimeException za sve vrednosti ulaznog parmetra koje su van opsega 135-355
+	 */
 	public void setSirina(int sirina) {
 	if (sirina < 135 && sirina > 355)
 	throw new RuntimeException("Sirina van opsega");
@@ -38,18 +69,31 @@ public class AutoGuma {
 	public int getVisina() {
 	return visina;
 	}
+	/**
+	 * Postavlja vrednost visine gume.
+	 * @param visina
+	 * @throws java.lang.RuntimeException za sve vrednosti ulaznog parmetra koje su van opsega 25-95
+	 */
 	public void setVisina(int visina) {
 	if (visina < 25 || visina > 95)
 	throw new RuntimeException("Visina van opsega");
 	this.visina = visina;
 	}
 	
+	/**
+	 * Redefinisana metoda toString, za formatirani ispis 	
+	 * @return String sa vrednostima svih atrubuta 
+	 */
 	@Override
 	public String toString() {
 	return "AutoGuma marke:" + markaModel + ", precnika: " + precnik +
 	", sirine: " + sirina + ", visine:" + visina;
 	}
-	
+	/**
+	 * Redefinisana metoda equals, za poredjenje dva objekta klase AutoGuma
+	 * @param obj(objekat)
+	 * @return true ako su poredjeni objekti jednaki ili false ako nisu jednaki ili ulazni objekat nije instanca klase AutoGuma
+	 */
 	@Override
 	public boolean equals(Object obj) {
 	if (this == obj)
